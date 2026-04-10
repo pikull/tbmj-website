8e27a9d95dea9557bb4b6eb794768bbeae81d5b9
Atharv <atharvrajure@gmail.com>
2026-04-09 17:38:22 -0700

    fixed problem on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="mx-auto max-w-screen-2xl px-6 md:px-10 py-12">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8D-QErPzFzNTmcPoxRR1EKcVdFycAhGfyMuYrxLdgt6mUtw/viewform?embedded=true" width="640" height="1588" frameBorder="0" marginHeight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8D-QErPzFzNTmcPoxRR1EKcVdFycAhGfyMuYrxLdgt6mUtw/viewform?embedded=true" width="640" height="1588">Loading…</iframe>
     </div>
   );
 }
